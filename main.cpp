d125b86479c5d35ed483927ffc203cb6b73bfbaa
RROP <kimjiho1121@naver.com>
2025-03-20 14:03:38 +0900

    init repo

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <libevdev-1.0/libevdev/libevdev.h>
+#include <unistd.h>
+#include <cmath>
+#include <cstring>
+
+
+using TIMETYPE= unsigned long;
+
+constexpr TIMETYPE BOUNCING_TIME_MICRO_SECOND = 50000;
+constexpr auto EVSZ = sizeof(input_event);
+int main(int argc, char* argv[])
+{
+    input_event evS;
+
+    timeval inTimeTable[256] = {0, };
+    while (true){
+        ssize_t bytesRead = read(STDIN_FILENO, &evS, EVSZ);
+        if (bytesRead != EVSZ){
+            continue;
+        }
+        if (evS.type == EV_KEY && evS.value == 1){
+            if((evS.time.tv_sec - inTimeTable[evS.code].tv_sec < 2) && evS.time.tv_usec !=  inTimeTable[evS.code].tv_usec){
+                long time  = evS.time.tv_sec - inTimeTable[evS.code].tv_sec;
+                time *= 1000000;
+                if((evS.time.tv_usec + time) - inTimeTable[evS.code].tv_usec < BOUNCING_TIME_MICRO_SECOND){
+                    continue;
+                }
+            }
+            inTimeTable[evS.code] = evS.time;
+            
+        }
+        write(STDOUT_FILENO, &evS, EVSZ);
+    }
+    return 0;
+}
